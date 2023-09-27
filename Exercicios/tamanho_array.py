e93d9da0ca57f1f7a32395c1e37df55fc7613281
José Carlos da Silva Souza <74185292+josecarlossouza@users.noreply.github.com>
2023-09-27 11:09:22 -0300

    Add files via upload

---
+++ Exercicios/tamanho_array.py
@@ -0,0 +1,16 @@
+# Copie o array apresentado embaixo no seu editor de código, 
+# e imprima no terminal: a quantidade de elementos que ele possui, 
+# o dado salvo no índice 2, o dado salvo no índice 9, e dado salvo no índice 14.
+# Trabalhe esse código no Google Colab, 
+# e compartilhe o link do projeto no campo ao lado para que outros desenvolvedores possam analisá-lo.  
+
+
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+print(f"\nQuantidade de elementos:\n\n{len(lista_musicos)}");
+
+print(f"\nNome músico no índice 2 do array:\n\n{lista_musicos[2]}");
+
+print(f"\nNome músico no índice 9 do array:\n\n{lista_musicos[9]}");
+
+print(f"\nNome músico no índice 14 do array:\n\n{lista_musicos[14]}\n");
