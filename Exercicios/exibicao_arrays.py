83baf6a0d238658dccc839bb6f1299d1a7092b52
José Carlos da Silva Souza <74185292+josecarlossouza@users.noreply.github.com>
2023-09-27 10:31:35 -0300

    Add files via upload

---
+++ Exercicios/exibicao_arrays.py
@@ -0,0 +1,16 @@
+# Declare dois arrays, cada um com um mínimo de cinco elementos, 
+# e imprima eles no terminal usando o comando print(). 
+# O primeiro array deve conter os produtos de uma loja da sua escolha 
+# (loja de comida, materiais de construção, música, etc). 
+# O segundo array deve conter os anos de nascimento de familiares e amigos seus. 
+# Lembre-se de usar nomes descritivos para nomear cada variável, 
+# e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).
+
+produtos = ['Maçã', 'Banana', 'Goiaba', 'Melão', 'Mamão'];
+
+anos = [1941, 1958, 1986, 1988, 2020];
+
+
+print(f"\nLista de produtos no array: \n\n{produtos}");
+
+print(f"\nAnos de nascimento de meus familiares: \n\n{anos} \n");
